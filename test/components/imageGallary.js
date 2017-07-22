65b374cb03ed8ae08bcb3e07f195c69072d6f8ae
Lakha Singh Nagi <polizic@yahoo.co.in>
2017-07-22 14:30:49 +0200

    - Added npm test command in package.json with test server config. - Added test case for ImageGallary component as imageGallary.js with failing test cases as per requirements.

---
+++ test/components/imageGallary.js
@@ -0,0 +1,21 @@
+
+import {expect} from 'chai';
+import sinon from 'sinon';
+
+describe('ImageGallary component', function(){
+	it('should render when initialized', function(){
+		expect(false).to.equal(true);
+	});	
+
+	it('should show next image when next putton is clicked', function(){
+		expect(false).to.equal(true);
+	});
+
+	it('should show previous image when prev putton is clicked', function(){
+		expect(false).to.equal(true);
+	});
+
+	it('should update indicator value when image in gallary is updated', function(){
+		expect(false).to.equal(true);
+	});
+});
